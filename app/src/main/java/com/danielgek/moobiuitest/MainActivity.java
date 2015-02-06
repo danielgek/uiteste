529318fc2ebebcfc6acd181e79889967399e8580
danielgek <danielgek@gmail.com>
2015-02-06 12:51:03 +0000

    use fragments

--- app/src/main/java/com/danielgek/moobiuitest/MainActivity.java
+++ app/src/main/java/com/danielgek/moobiuitest/MainActivity.java
@@ -1,6 +1,10 @@
 package com.danielgek.moobiuitest;
 
 import android.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
@@ -13,6 +17,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.danielgek.moobiuitest.fragments.Fragment1;
+import com.danielgek.moobiuitest.fragments.Fragment2;
 import com.danielgek.moobiuitest.view.SlidingTabLayout;
 
 
@@ -34,7 +40,10 @@ public class MainActivity extends ActionBarActivity {
 
 
         mViewPager = (ViewPager) findViewById(R.id.viewpager);
-        mViewPager.setAdapter(new SamplePagerAdapter());
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        mViewPager.setAdapter(new SamplePagerAdapter(fragmentManager));
         // END_INCLUDE (setup_viewpager)
 
         // BEGIN_INCLUDE (setup_slidingtablayout)
@@ -73,68 +82,37 @@ public class MainActivity extends ActionBarActivity {
      * this class is the {@link #getPageTitle(int)} method which controls what is displayed in the
      * {@link SlidingTabLayout}.
      */
-    class SamplePagerAdapter extends PagerAdapter {
+    class SamplePagerAdapter extends FragmentStatePagerAdapter {
 
-        /**
-         * @return the number of pages to display
-         */
+        public SamplePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+
         @Override
         public int getCount() {
             return 3;
         }
 
-        /**
-         * @return true if the value returned from {@link #instantiateItem(android.view.ViewGroup, int)} is the
-         * same object as the {@link android.view.View} added to the {@link android.support.v4.view.ViewPager}.
-         */
-        @Override
-        public boolean isViewFromObject(View view, Object o) {
-            return o == view;
-        }
 
-        // BEGIN_INCLUDE (pageradapter_getpagetitle)
-        /**
-         * Return the title of the item at {@code position}. This is important as what this method
-         * returns is what is displayed in the {@link SlidingTabLayout}.
-         * <p>
-         * Here we construct one using the position value, but for real application the title should
-         * refer to the item's contents.
-         */
+
+
         @Override
         public CharSequence getPageTitle(int position) {
             return "Item " + (position + 1);
         }
-        // END_INCLUDE (pageradapter_getpagetitle)
 
-        /**
-         * Instantiate the {@link android.view.View} which should be displayed at {@code position}. Here we
-         * inflate a layout from the apps resources and then change the text view to signify the position.
-         */
+
         @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            // Inflate a new layout from our resources
-            View view = getLayoutInflater().inflate(R.layout.pager_item,container, false);
-            // Add the newly created View to the ViewPager
-            container.addView(view);
+        public Fragment getItem(int position) {
+            Fragment fragment = null;
+            switch (position){
+                case 0: fragment = new Fragment1();break;
+                case 1: fragment = new Fragment2();break;
+                default: fragment = new Fragment1();
+            }
 
-            // Retrieve a TextView from the inflated View, and update it's text
-            TextView title = (TextView) view.findViewById(R.id.item_title);
-            title.setText(String.valueOf(position + 1));
-
-            Log.i(LOG_TAG, "instantiateItem() [position: " + position + "]");
-
-            // Return the View
-            return view;
-        }
-
-        /**
-         * Destroy the item from the {@link android.support.v4.view.ViewPager}. In our case this is simply removing the
-         * {@link android.view.View}.
-         */
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView((View) object);
-            Log.i(LOG_TAG, "destroyItem() [position: " + position + "]");
+            return fragment;
         }
 
     }
